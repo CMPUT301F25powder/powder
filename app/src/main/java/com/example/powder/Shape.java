d7d63a664b920457d114fcd2e50913a103389fb5
Ezra Ho <x.crazy.craft99@gmail.com>
2025-09-24 14:56:25 -0600

    added color in shape

--- app/src/main/java/com/example/powder/Shape.java
+++ app/src/main/java/com/example/powder/Shape.java
@@ -3,4 +3,5 @@ package com.example.powder;
 public class Shape {
     private int x;
     private int y;
+    private String color = "blue";
 }
--- app/src/main/java/com/example/powder/Square.java
+++ app/src/main/java/com/example/powder/Square.java
@@ -1,5 +1,5 @@
 package com.example.powder;
 
 public class Square extends Shape {
-    
+
 }
